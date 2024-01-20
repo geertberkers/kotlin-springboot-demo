b1a89ad35db2d5e3d438834f9bc29c2022c14e8b
Geert Berkers <geertberkers@hotmail.com>
2024-01-20 05:26:42 +0100

    Added GIT to test spring boot
    See a lot of things which just look like I did earlier with KituraSwift.
    Its a project configuration, including some dependencies, a framework, and a template to show html.
    Also some extension functions to help
    We can add some routes, and eventually a database

---
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -0,0 +1,16 @@
+package com.example.demo
+
+import org.springframework.boot.Banner
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BlogApplication
+
+
+
+fun main(args: Array<String>) {
+	runApplication<BlogApplication>(*args) {
+		setBannerMode(Banner.Mode.OFF)
+	}
+}
